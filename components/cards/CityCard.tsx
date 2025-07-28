4b6a8d28208207711bc342a099fc6177ecae23b4
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-07-28 19:07:36 +0500

    city page

--- components/cards/CityCard.tsx
+++ components/cards/CityCard.tsx
@@ -15,10 +15,10 @@ const CityCard = ({ image, description, place, city }: CityCardProps) => {
       className="basis-2/5 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 pl-4"
     >
       <img src={image} alt={description} className="rounded" />
-      <p className="font-semibold text-gray-700 mt-2 leading-tight md:max-w-32">
+      <p className="font-halyard-text text-[#444444] mt-2 leading-tight md:max-w-32">
         {description}
       </p>
-      <p className="text-gray-500 text-sm mt-1">{place}</p>
+      <p className="text-[#666666] font-halyard-text-light text-sm mt-1">{place}</p>
     </Link>
   );
 };
